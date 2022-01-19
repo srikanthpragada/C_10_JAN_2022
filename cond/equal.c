54cf2bd4458e6d29403ce40e024e1a638642ba5e
srikanthpragada <srikanthpragada@gmail.com>
2022-01-19 18:55:15 +0530

    19-JAN

---
+++ cond/equal.c
@@ -0,0 +1,17 @@
+// Display whether two numbers are equal
+
+#include <stdio.h>
+
+void main()
+{
+  int n1,n2;
+
+    printf("Enter two numbers  : ");
+    scanf("%d%d", &n1,&n2);
+
+    if (n1 == n2)
+        printf("Equal");
+    else
+        printf("Not Equal");
+
+}
